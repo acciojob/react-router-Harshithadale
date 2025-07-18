7ae62790803be8a619c21be30a43dd4f4173d3ae
harshithadale <daleharshi6045@gmail.com>
2025-07-18 14:22:31 +0530

    initial-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <br />
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
